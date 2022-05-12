3383bc7cf855a0b0fc4576994c1d32f35f1849ad
Max <61512470+Mevial@users.noreply.github.com>
2022-05-12 22:41:27 +0300

    fix esLint

--- src/app/state/effect/app.effects.ts
+++ src/app/state/effect/app.effects.ts
@@ -3,8 +3,5 @@ import { Actions } from '@ngrx/effects';
 
 @Injectable()
 export class AppEffects {
-  /*TODO Delete ES Lint Ignore after implementing state manager basic components*/
-
-  // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
   constructor(private actions$: Actions) {}
 }
