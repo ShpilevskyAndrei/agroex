c44048644f75fc8caafa47ec40c126e22e318874
Igor Nichiporuk <ig.nichiporuk@gmail.com>
2022-05-18 23:02:06 +0200

    remove unnecessary

--- src/app/pages/registration-page/registration-page.component.ts
+++ src/app/pages/registration-page/registration-page.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-registration-page',
   templateUrl: './registration-page.component.html',
-  styleUrls: ['./registration-page.component.scss']
+  styleUrls: ['./registration-page.component.scss'],
 })
-export class RegistrationPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class RegistrationPageComponent {}
