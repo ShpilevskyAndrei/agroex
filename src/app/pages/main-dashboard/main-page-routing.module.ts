11995a32e840afafbac39e653df652daeb9b701d
Igor Nichiporuk <ig.nichiporuk@gmail.com>
2022-05-18 21:56:22 +0200

    add routing

---
+++ src/app/pages/main-dashboard/main-page-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { MainPageComponent } from './main-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent,
+  },
+];
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class MainPageRoutingModule {}
---
+++ src/app/pages/main-dashboard/main-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-dashboard',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.scss'],
+})
+export class MainPageComponent {}
