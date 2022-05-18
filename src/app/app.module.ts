6d3674e7f5a4bd27841611cd6028fb1b7190ef28
Igor Nichiporuk <ig.nichiporuk@gmail.com>
2022-05-18 10:08:09 +0200

    remove unnecessary imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,6 @@ import { HeaderModule } from './shared/header/header.module';
 import { reducers, metaReducers } from './state/reducer';
 import { environment } from '../environments/environment';
 import { AppEffects } from './state/effect/app.effects';
-import { MatButtonModule } from "@angular/material/button";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   declarations: [AppComponent],
@@ -32,9 +29,6 @@ import { MatInputModule } from "@angular/material/input";
       logOnly: environment.production,
     }),
     EffectsModule.forRoot([AppEffects]),
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
   ],
   bootstrap: [AppComponent],
 })
