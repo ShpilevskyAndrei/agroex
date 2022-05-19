e12057813fd5cc04f8c160c800b9397bfd046861
Max <61512470+Mevial@users.noreply.github.com>
2022-05-19 20:53:26 +0300

    fix error-container

--- src/app/pages/error-page/error-page-container.component.ts
+++ src/app/pages/error-page/error-page-container.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-error-page-container',
-  template: ` <app-error-page-container></app-error-page-container>`,
+  template: `<app-error-page-container></app-error-page-container>`,
 })
 export class ErrorPageContainerComponent {}
