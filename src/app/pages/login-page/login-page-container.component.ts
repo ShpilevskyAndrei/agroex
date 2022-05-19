5d7a6c31a370ea9dee59e4ab2998e4526de68185
Max <61512470+Mevial@users.noreply.github.com>
2022-05-19 20:59:58 +0300

    fix app-login

--- src/app/pages/login-page/login-page-container.component.ts
+++ src/app/pages/login-page/login-page-container.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login-page-container',
-  template: ` <app-login-page></app-login-page>`,
+  template: `<app-login-page></app-login-page>`,
 })
 export class LoginPageContainerComponent {}
