54ad255a02ec984bcf3d3ced6f63105778c605fa
Max <61512470+Mevial@users.noreply.github.com>
2022-05-30 17:30:36 +0300

    add opportunity to view user\logout in url createadvertisement

--- src/app/pages/create-advertisement-page/create-advertisement-page-container.component.ts
+++ src/app/pages/create-advertisement-page/create-advertisement-page-container.component.ts
@@ -1,7 +1,26 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { RegistrationPageActions } from '../../state/registration-page/registration-page.actions';
+import { selectUserData } from '../../state/registration-page/registration-page.selectors';
+import { IUser } from '../registration-page/interfaces/user-api-response.interface';
 
 @Component({
   selector: 'app-create-advertisement-page-container',
-  template: ` <app-create-advertisement-page></app-create-advertisement-page>`,
+  template: ` <app-create-advertisement-page
+    [user]="user$ | async"
+    (logout)="onLogout()"
+  ></app-create-advertisement-page>`,
 })
-export class CreateAdvertisementPageContainerComponent {}
+export class CreateAdvertisementPageContainerComponent {
+  public user$: Observable<IUser | null>;
+
+  constructor(private store: Store) {
+    this.user$ = this.store.select(selectUserData);
+  }
+
+  public onLogout(): void {
+    this.store.dispatch(RegistrationPageActions.getUserLogout());
+  }
+}
--- src/app/pages/create-advertisement-page/create-advertisement-page.component.ts
+++ src/app/pages/create-advertisement-page/create-advertisement-page.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import { IUser } from '../registration-page/interfaces/user-api-response.interface';
 
 @Component({
   selector: 'app-create-advertisement-page',
   templateUrl: './create-advertisement-page.component.html',
   styleUrls: ['./create-advertisement-page.component.scss'],
 })
-export class CreateAdvertisementPageComponent {}
+export class CreateAdvertisementPageComponent {
+  @Input() public user: IUser | null;
+
+  @Output() public logout: EventEmitter<void> = new EventEmitter<void>();
+
+  public onLogout(): void {
+    this.logout.emit();
+  }
+}
