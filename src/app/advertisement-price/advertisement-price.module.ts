5bc5a9fd17aef8ba029bb7f62f86e1cd5a4c0cb9
Igor Nichiporuk <ig.nichiporuk@gmail.com>
2022-05-30 14:52:24 +0200

    index on avertisement-comp: da4c4764 Merge pull request #25 from ShpilevskyAndrei/fix-styles

---
+++ src/app/advertisement-price/advertisement-price.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdvertisementPriceComponent } from './advertisement-price.component';
+
+@NgModule({
+  declarations: [AdvertisementPriceComponent],
+  imports: [CommonModule],
+  exports: [AdvertisementPriceComponent],
+})
+export class AdvertisementPriceModule {}
