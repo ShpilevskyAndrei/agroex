915e31269efbee8a13fcb3d96d90db25797013a9
Igor Nichiporuk <ig.nichiporuk@gmail.com>
2022-05-30 14:52:24 +0200

    index on avertisement-comp: da4c4764 Merge pull request #25 from ShpilevskyAndrei/fix-styles

---
+++ src/app/advertisement-price/advertisement-price.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advertisement-price',
+  templateUrl: './advertisement-price.component.html',
+  styleUrls: ['./advertisement-price.component.scss'],
+})
+export class AdvertisementPriceComponent {}
