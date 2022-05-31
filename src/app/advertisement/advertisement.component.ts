d50e9fb8f3c75029760afa533d4e3c604d0b0001
Igor Nichiporuk <ig.nichiporuk@gmail.com>
2022-05-31 16:07:37 +0200

    add advertisement with style

--- src/app/advertisement/advertisement.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-advertisement',
-  templateUrl: './advertisement.component.html',
-  styleUrls: ['./advertisement.component.scss'],
-})
-export class AdvertisementComponent {}
