016926992051b56b888e8e4237a89f8037d5ef14
Igor Nichiporuk <ig.nichiporuk@gmail.com>
2022-05-31 16:07:37 +0200

    add advertisement with style

--- src/app/advertisement-title/advertisement-title.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-advertisement-title',
-  templateUrl: './advertisement-title.component.html',
-  styleUrls: ['./advertisement-title.component.scss'],
-})
-export class AdvertisementTitleComponent {}
