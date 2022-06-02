7d24cf68b86e7aaaa99eebaad5aceb78c487347e
Igor Nichiporuk <ig.nichiporuk@gmail.com>
2022-06-02 12:20:07 +0200

    created pipe to format date

--- src/app/advertisements-list/advertisement/advertisement-price/advertisement-price.module.ts
+++ src/app/advertisements-list/advertisement/advertisement-price/advertisement-price.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 
 import { AdvertisementPriceComponent } from './advertisement-price.component';
+import { AvailableDateFormatPipe } from '../../../shared/pipes/available-date-format.pipe';
 
 @NgModule({
-  declarations: [AdvertisementPriceComponent],
+  declarations: [AdvertisementPriceComponent, AvailableDateFormatPipe],
   imports: [CommonModule, MatIconModule],
   exports: [AdvertisementPriceComponent],
 })
