c7e105661459121226d0017fe516a47bb2a9aeee
Igor Nichiporuk <ig.nichiporuk@gmail.com>
2022-06-02 12:20:07 +0200

    created pipe to format date

--- src/app/advertisements-list/advertisement/advertisement-title/advertisement-title.module.ts
+++ src/app/advertisements-list/advertisement/advertisement-title/advertisement-title.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdvertisementTitleComponent } from './advertisement-title.component';
+import { CreatDateFormatPipe } from '../../../shared/pipes/creat-date-format.pipe';
 
 @NgModule({
-  declarations: [AdvertisementTitleComponent],
+  declarations: [AdvertisementTitleComponent, CreatDateFormatPipe],
   imports: [CommonModule],
   exports: [AdvertisementTitleComponent],
 })
