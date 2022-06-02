997b375f9483252793caf58393307746e289c487
Max <61512470+Mevial@users.noreply.github.com>
2022-06-02 11:04:47 +0300

    create reducer, action

--- src/app/state/create-advertisement-page/create-advertisement-page.actions.ts
+++ src/app/state/create-advertisement-page/create-advertisement-page.actions.ts
@@ -1,1 +1,19 @@
-export const CreateAdvertisementPageActions = {};
+import { HttpErrorResponse } from '@angular/common/http';
+import { createAction, props } from '@ngrx/store';
+import { IAdvertisementFormCredentials } from '../../pages/create-advertisement-page/interfaces/create-advertisement.interface';
+
+export const CreateAdvertisementPageActions = {
+  createAdvertisementRequest: createAction(
+    '[CREATE_ADVERTISEMENT_PAGE] advertisement requested',
+    props<{ formAdvertisement: IAdvertisementFormCredentials }>()
+  ),
+
+  createAdvertisementSuccess: createAction(
+    '[CREATE_ADVERTISEMENT_PAGE] advertisement success'
+  ),
+
+  createAdvertisementError: createAction(
+    '[CREATE_ADVERTISEMENT_PAGE] advertisement error',
+    props<{ error: HttpErrorResponse }>()
+  ),
+};
