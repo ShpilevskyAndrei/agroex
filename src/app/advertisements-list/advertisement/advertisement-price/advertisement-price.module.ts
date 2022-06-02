2e305271624b6c3301a0c2d2407ba6e6a35e5ddd
Igor Nichiporuk <ig.nichiporuk@gmail.com>
2022-06-02 22:00:12 +0200

    currency format

--- src/app/advertisements-list/advertisement/advertisement-price/advertisement-price.module.ts
+++ src/app/advertisements-list/advertisement/advertisement-price/advertisement-price.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+registerLocaleData(localeFr, 'fr');
 
 import { AdvertisementPriceComponent } from './advertisement-price.component';
 import { AvailableDateFormatPipe } from '../../../shared/pipes/available-date-format.pipe';
