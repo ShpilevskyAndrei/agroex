0d9aa7972ef906b3c0d8378ebc1eb04782f2867a
Max <61512470+Mevial@users.noreply.github.com>
2022-06-06 14:29:03 +0300

    implement spinner to categories

--- src/app/pages/main-dashboard/categories/categories.module.ts
+++ src/app/pages/main-dashboard/categories/categories.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatListModule } from '@angular/material/list';
 import { NgModule } from '@angular/core';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { CategoriesComponent } from './categories.component';
 import { CategoriesService } from './categories.service';
@@ -9,7 +10,13 @@ import { CategoryModule } from './category/category.module';
 
 @NgModule({
   declarations: [CategoriesComponent],
-  imports: [CommonModule, CategoryModule, MatTabsModule, MatListModule],
+  imports: [
+    CommonModule,
+    CategoryModule,
+    MatTabsModule,
+    MatListModule,
+    NgxSpinnerModule,
+  ],
   providers: [CategoriesService],
   exports: [CategoriesComponent],
 })
