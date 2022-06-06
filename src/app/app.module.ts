255a0804cc11e2622e9d3322e49def28a165c28d
Max <61512470+Mevial@users.noreply.github.com>
2022-06-06 21:53:25 +0300

    fx app module, delete helper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -13,26 +13,10 @@ import { AppComponent } from './app.component';
 import { MainDashboardModule } from './pages/main-dashboard/main-dashboard.module';
 import { AppRoutingModule } from './app-routing.module';
 import { ErrorPageModule } from './pages/error-page/error-page.module';
-import { InitialLoaderHelper } from './shared/helpers/initial-loader.helper';
 import { IconSerializeService } from './shared/services/icon-serialize.service';
 import { StateModule } from './state/state.module';
 import { APPEARANCE } from './shared/constants/appearance-outline';
 import { AuthGuard } from './auth.guard';
-
-export function appLoader() {
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-  return () => {
-    // @ts-ignore
-    new InitialLoaderHelper(() => {
-      console.log('31313');
-    });
-    return new Promise<void>((res) => {
-      setTimeout(() => {
-        res();
-      }, 3000);
-    }).then(() => InitialLoaderHelper.remove());
-  };
-}
 
 @NgModule({
   declarations: [AppComponent, AppContainerComponent],
@@ -57,7 +41,6 @@ export function appLoader() {
       provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
       useValue: APPEARANCE,
     },
-    { provide: APP_INITIALIZER, useFactory: appLoader, multi: true },
   ],
   bootstrap: [AppContainerComponent],
 })
