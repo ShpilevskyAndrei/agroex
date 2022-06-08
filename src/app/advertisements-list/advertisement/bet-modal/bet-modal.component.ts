9ed256c0980c4dafdb898d40b2b8676d8c87fb9c
Igor Nichiporuk <ig.nichiporuk@gmail.com>
2022-06-08 10:47:11 +0200

    add modal

--- src/app/advertisements-list/advertisement/bet-modal/bet-modal.component.ts
+++ src/app/advertisements-list/advertisement/bet-modal/bet-modal.component.ts
@@ -1,8 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { BetValidators } from './intefaces/bet-validator';
+
+export interface DialogData {
+  bet: string;
+  price: string;
+  currency: string;
+}
 
 @Component({
   selector: 'app-bet-modal',
   templateUrl: './bet-modal.component.html',
   styleUrls: ['./bet-modal.component.scss'],
 })
-export class BetModalComponent {}
+export class BetModalComponent {
+  public betForm: FormGroup = new FormGroup({
+    bet: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.maxLength(9),
+        BetValidators.passwordsMatching('', this.data.price),
+      ],
+      updateOn: 'change',
+    }),
+  });
+
+  constructor(
+    public dialogRef: MatDialogRef<BetModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
+
+  public onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  public get(key: string): FormControl {
+    return this.betForm.get(key) as FormControl;
+  }
+}
