dc5fa2f95a7b1d654546cc0b1f4cb12093bf3e04
ShpilevskyAndrei <103843433+ShpilevskyAndrei@users.noreply.github.com>
2022-06-09 16:24:12 +0300

    added properties of ads to ad pages

--- src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IAdvertisementInterface } from '../../../advertisements-list/interfaces/advertisement.interface';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.scss'],
 })
-export class BreadcrumbsComponent {}
+export class BreadcrumbsComponent {
+  @Input() public advertisement: IAdvertisementInterface;
+}
