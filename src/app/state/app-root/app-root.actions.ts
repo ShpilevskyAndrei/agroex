00702616efad8f35733d6c7bbb80dca410689dfb
Max <61512470+Mevial@users.noreply.github.com>
2022-06-13 19:33:09 +0300

    add action

--- src/app/state/app-root/app-root.actions.ts
+++ src/app/state/app-root/app-root.actions.ts
@@ -1,1 +1,9 @@
-export const AppRootActions = {};
+import { createAction, props } from '@ngrx/store';
+import { UserPanelOptionId } from '../../shared/components/header/enums/user-panel-option-id';
+
+export const AppRootActions = {
+  getUserSelectTab: createAction(
+    '[APP_ROOT] myAccount success',
+    props<{ selectedOptionId: UserPanelOptionId }>()
+  ),
+};
