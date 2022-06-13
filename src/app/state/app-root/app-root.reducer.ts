71d9118fd80dc09e4f2a5cd2190a0abbb7de4433
Max <61512470+Mevial@users.noreply.github.com>
2022-06-13 19:33:16 +0300

    add reducer

--- src/app/state/app-root/app-root.reducer.ts
+++ src/app/state/app-root/app-root.reducer.ts
@@ -1,9 +1,30 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
 
-export interface AppRootState {}
+import { UserPanelOptionId } from '../../shared/components/header/enums/user-panel-option-id';
+import { DEFAULT_LOADING_STATUS } from '../../shared/constants/lodaing-default-status';
+import { LoadingStatus } from '../../shared/interfaces/loading-status';
+import { AppRootActions } from './app-root.actions';
+
+export interface AppRootState {
+  appRootLoadingStatus: LoadingStatus;
+  selectedOptionId: UserPanelOptionId;
+}
 
 export const APP_ROOT = 'appRoot';
 
-const initialState = {};
+const initialState = {
+  appRootLoadingStatus: DEFAULT_LOADING_STATUS,
+  selectedOptionId: UserPanelOptionId.MyAccount,
+};
 
-export const APP_ROOT_REDUCER = createReducer(initialState);
+export const APP_ROOT_REDUCER = createReducer(
+  initialState,
+  on(
+    AppRootActions.getUserSelectTab,
+    (state, { selectedOptionId }): AppRootState => ({
+      ...state,
+      selectedOptionId,
+      appRootLoadingStatus: DEFAULT_LOADING_STATUS,
+    })
+  )
+);
