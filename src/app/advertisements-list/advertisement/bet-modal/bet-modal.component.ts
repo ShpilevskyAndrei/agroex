006f8a7525aa9a76bc2d34f8155c155bc7d45475
Igor Nichiporuk <ig.nichiporuk@gmail.com>
2022-06-14 19:06:14 +0200

    remove ngModel binding

--- src/app/advertisements-list/advertisement/bet-modal/bet-modal.component.ts
+++ src/app/advertisements-list/advertisement/bet-modal/bet-modal.component.ts
@@ -1,18 +1,26 @@
-import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Inject,
+  OnInit,
+} from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { Subscription, tap } from 'rxjs';
 
 import { BetValidators } from './intefaces/bet-validator';
 import { BetModalDataInterface } from './intefaces/bet-modal-data.interface';
 import { CurrenciesEnum } from './enums/currencies.enum';
 
+@UntilDestroy()
 @Component({
   selector: 'app-bet-modal',
   templateUrl: './bet-modal.component.html',
   styleUrls: ['./bet-modal.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BetModalComponent {
+export class BetModalComponent implements OnInit {
   public betForm: FormGroup = new FormGroup({
     bet: new FormControl('', {
       validators: [
@@ -28,13 +36,31 @@ export class BetModalComponent {
   });
 
   public currenciesEnum = CurrenciesEnum;
+  public betValueChangesSubscription: Subscription | undefined;
+  public betValue: string | undefined;
 
   constructor(
     private dialogRef: MatDialogRef<BetModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: BetModalDataInterface
   ) {}
 
+  public ngOnInit(): void {
+    this.betValueChangesSubscription = this.betForm
+      .get('bet')
+      ?.valueChanges.pipe(
+        tap((result) => {
+          this.betValue = result;
+        }),
+        untilDestroyed(this)
+      )
+      .subscribe();
+  }
+
   public onCloseBetModal(): void {
     this.dialogRef.close(this.betForm.get('bet')?.value);
+  }
+
+  public onCloseModal(): void {
+    this.dialogRef.close();
   }
 }
