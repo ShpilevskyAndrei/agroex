b7fcb6d8ceeacdf2513f0231fd63029189541647
Max <61512470+Mevial@users.noreply.github.com>
2022-06-15 21:09:49 +0300

    fix account-page effects

--- src/app/state/account-page/account-page.effects.ts
+++ src/app/state/account-page/account-page.effects.ts
@@ -2,23 +2,21 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { catchError, map, of, switchMap } from 'rxjs';
+import { catchError, map, of, switchMap, withLatestFrom } from 'rxjs';
 
 import { AccountPageService } from '../../pages/account-page/services/account-page.service';
 import { IAdvertisementRequestInterface } from '../../shared/components/advertisements-list/interfaces/advertisement-request.interface';
+import { selectUserToken } from '../registration-page/registration-page.selectors';
 import { AccountPageActions } from './account-page.actions';
 
 @Injectable()
 export class AccountPageEffects {
   public myAdvertisements$ = createEffect(() => {
     return this.actions$.pipe(
-      ofType(
-        AccountPageActions.getMyAdvertisementsRequest,
-        AccountPageActions.getMyAdvertisementsSuccess,
-        AccountPageActions.getMyAdvertisementsError
-      ),
-      switchMap(() =>
-        this.accountPageService.getMyAdvertisements().pipe(
+      ofType(AccountPageActions.getMyAdvertisementsRequest),
+      withLatestFrom(this.store.select(selectUserToken)),
+      switchMap(([_, selectUserToken]) =>
+        this.accountPageService.getMyAdvertisements(selectUserToken).pipe(
           map((myAdvertisements: IAdvertisementRequestInterface) =>
             AccountPageActions.getMyAdvertisementsSuccess({
               myAdvertisements,
