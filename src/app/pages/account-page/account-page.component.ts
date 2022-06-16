cd9c1492a1be688564b9e982151d18cfe261c594
Max <61512470+Mevial@users.noreply.github.com>
2022-06-16 18:36:10 +0300

    add new function get combineLoadingStatus

--- src/app/pages/account-page/account-page.component.ts
+++ src/app/pages/account-page/account-page.component.ts
@@ -5,6 +5,7 @@ import {
   Input,
   Output,
 } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 import { UserPanelOptionId } from '../../shared/components/header/enums/user-panel-option-id';
 import { IUser } from '../../shared/interfaces/user.interface';
@@ -33,9 +34,44 @@ export class AccountPageComponent {
     new EventEmitter<UserPanelOptionId>();
   @Output() public dispatcher: EventEmitter<Function> =
     new EventEmitter<Function>();
+  @Output() public confirmDeal: EventEmitter<string> =
+    new EventEmitter<string>();
 
   public userPanelOption: IUserOptionsType[] = USER_PANEL_OPTION;
   public userPanelOptionId = UserPanelOptionId;
+
+  constructor(private spinner: NgxSpinnerService) {}
+
+  public get combineLoadingStatus(): LoadingStatus {
+    const loadingStatusArr: (LoadingStatus | null)[] = [
+      this.myAdvertisementsLoadingStatus,
+    ];
+    if (
+      loadingStatusArr.every(
+        (loadingStatus: LoadingStatus | null) => loadingStatus?.loaded
+      )
+    ) {
+      return { loaded: true, loading: false, error: null };
+    }
+
+    if (
+      loadingStatusArr.some(
+        (loadingStatus: LoadingStatus | null) => loadingStatus?.loading
+      )
+    ) {
+      this.spinner.show();
+      return { loaded: false, loading: true, error: null };
+    }
+
+    return {
+      loaded: false,
+      loading: false,
+      error:
+        loadingStatusArr.find(
+          (loadingStatus: LoadingStatus | null) => loadingStatus?.error
+        )?.error || null,
+    };
+  }
 
   public onLogout(): void {
     this.logout.emit();
@@ -47,5 +83,9 @@ export class AccountPageComponent {
 
   public onDispatcher(dispatcher: Function): void {
     this.dispatcher.emit(dispatcher);
+  }
+
+  public onConfirmDeal(slug: string): void {
+    this.confirmDeal.emit(slug);
   }
 }
