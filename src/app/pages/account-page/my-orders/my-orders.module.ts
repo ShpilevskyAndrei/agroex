27a7818d64d9ee0a7e556ee7ba1c7f9b0e8f40ba
Igor Nichiporuk <ig.nichiporuk@gmail.com>
2022-06-16 10:40:37 +0200

    add order template

--- src/app/pages/account-page/my-orders/my-orders.module.ts
+++ src/app/pages/account-page/my-orders/my-orders.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MyOrdersComponent } from './my-orders.component';
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
   declarations: [MyOrdersComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   exports: [MyOrdersComponent],
 })
 export class MyOrdersModule {}
