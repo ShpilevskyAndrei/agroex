06dd21ba956e9604e81b1289c051b211e44eefc0
Igor Nichiporuk <ig.nichiporuk@gmail.com>
2022-06-20 19:54:40 +0200

    add default settings (I got notification)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 
 import { TITLE } from './shared/constants/app-consts';
 import { IconSerializeService } from './shared/services/icon-serialize.service';
+import { AngularFireMessaging } from '@angular/fire/compat/messaging';
+import { getMessaging, getToken } from '@angular/fire/messaging';
+import { environment } from '../environments/environment';
+import { mergeMapTo } from 'rxjs';
+import { tap } from "rxjs/operators";
+import firebase from "firebase/compat";
+import MessagePayload = firebase.messaging.MessagePayload;
 
 @Component({
   selector: 'app-root',
@@ -10,6 +17,27 @@ import { IconSerializeService } from './shared/services/icon-serialize.service';
 })
 export class AppComponent {
   public title = TITLE;
+  public message: MessagePayload | null = null;
 
-  constructor(private iconSerialize: IconSerializeService) {}
+  constructor(
+    private iconSerialize: IconSerializeService,
+    private afMessaging: AngularFireMessaging
+  ) {
+    this.afMessaging.requestToken.subscribe(
+      (token) => {
+        console.log('Permission granted! Save to the server!', token);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+
+    this.afMessaging.messages
+      .pipe(
+        tap((message) => {
+          this.message = message;
+        })
+      )
+      .subscribe();
+  }
 }
