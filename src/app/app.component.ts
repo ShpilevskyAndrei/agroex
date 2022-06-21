0d49308735f62fdb4045aee59d47873008a6518c
Igor Nichiporuk <ig.nichiporuk@gmail.com>
2022-06-21 08:49:48 +0200

    remove unnecessary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
+import { AngularFireMessaging } from '@angular/fire/compat/messaging';
+import { catchError, throwError } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import firebase from 'firebase/compat';
+import MessagePayload = firebase.messaging.MessagePayload;
 
 import { TITLE } from './shared/constants/app-consts';
 import { IconSerializeService } from './shared/services/icon-serialize.service';
-import { AngularFireMessaging } from '@angular/fire/compat/messaging';
-import { getMessaging, getToken } from '@angular/fire/messaging';
-import { environment } from '../environments/environment';
-import { mergeMapTo } from 'rxjs';
-import { tap } from "rxjs/operators";
-import firebase from "firebase/compat";
-import MessagePayload = firebase.messaging.MessagePayload;
 
 @Component({
   selector: 'app-root',
@@ -23,14 +21,13 @@ export class AppComponent {
     private iconSerialize: IconSerializeService,
     private afMessaging: AngularFireMessaging
   ) {
-    this.afMessaging.requestToken.subscribe(
-      (token) => {
-        console.log('Permission granted! Save to the server!', token);
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
+    this.afMessaging.requestToken
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      )
+      .subscribe();
 
     this.afMessaging.messages
       .pipe(
