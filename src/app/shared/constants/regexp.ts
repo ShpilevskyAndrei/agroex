4526101c62762021a74807e36b507fcc6158653b
ShpilevskyAndrei <103843433+ShpilevskyAndrei@users.noreply.github.com>
2022-06-21 14:11:11 +0300

    drop in inputs is invalid value now

--- src/app/shared/constants/regexp.ts
+++ src/app/shared/constants/regexp.ts
@@ -1,4 +1,4 @@
 export const REGEXP_FOR_PHONE = /^([+]?[0-9\s-\(\)]{3,25})*$/i;
 export const REGEXP_FOR_EMAIL =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-export const REGEXP_FOR_IS_INTEGER_NUMBER = /^[0-9]{1,9}$/;
+export const REGEXP_FOR_IS_INTEGER_NUMBER = /^\d{1,9}$/;
