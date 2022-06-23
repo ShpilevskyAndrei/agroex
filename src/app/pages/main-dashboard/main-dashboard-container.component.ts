93cd164472d95a3b5e032422d7beeeaaab3d25e5
Igor Nichiporuk <ig.nichiporuk@gmail.com>
2022-06-23 09:25:35 +0200

    Revert "delete notification Input and Output"
    This reverts commit 5bb2c33b9c6be53443d19b0d264608a5aa641137.

--- src/app/pages/main-dashboard/main-dashboard-container.component.ts
+++ src/app/pages/main-dashboard/main-dashboard-container.component.ts
@@ -30,6 +30,7 @@ import {
   AdvertisementsListPageActions,
 } from '../../state/advertisements-list-page/advertisements-list-page.actions';
 import { UserRole } from '../../shared/components/header/enums/user-role';
+import { getNotificationMessage } from '../../state/app-root/app-root.selectors';
 
 @Component({
   selector: 'app-main-dashboard-container',
@@ -39,10 +40,12 @@ import { UserRole } from '../../shared/components/header/enums/user-role';
     [user]="user$ | async"
     [userRole]="userRole$ | async"
     [advertisementsRequest]="advertisementsRequest$ | async"
+    [notificationMessage]="notificationMessage$ | async"
     [advertisementsLoadingStatus]="advertisementsLoadingStatus$ | async"
     (logout)="onLogout()"
     (setBet)="onSetBet($event)"
     (selectTab)="onSelectTab($event)"
+    (addNotificationMessage)="onAddNotificationMessage($event)"
   ></app-main-dashboard>`,
 })
 export class MainDashboardContainerComponent implements OnInit {
@@ -52,6 +55,7 @@ export class MainDashboardContainerComponent implements OnInit {
   public userRole$: Observable<UserRole | null>;
   public advertisementsRequest$: Observable<IAdvertisementRequestInterface | null>;
   public advertisementsLoadingStatus$: Observable<LoadingStatus | null>;
+  public notificationMessage$: Observable<MessagePayload[] | null>;
 
   constructor(private store: Store, private spinner: NgxSpinnerService) {
     this.categories$ = this.store.select(selectCategoriesData);
@@ -64,6 +68,7 @@ export class MainDashboardContainerComponent implements OnInit {
     this.advertisementsLoadingStatus$ = this.store.select(
       selectAdvertisementsLoadingStatus
     );
+    this.notificationMessage$ = this.store.select(getNotificationMessage);
   }
 
   public ngOnInit(): void {
@@ -94,5 +99,9 @@ export class MainDashboardContainerComponent implements OnInit {
 
   public onSelectTab(selectedOptionId: UserPanelOptionId): void {
     this.store.dispatch(AppRootActions.getUserSelectTab({ selectedOptionId }));
+  }
+
+  public onAddNotificationMessage(message: MessagePayload): void {
+    this.store.dispatch(AppRootActions.getNotificationMessage({ message }));
   }
 }
