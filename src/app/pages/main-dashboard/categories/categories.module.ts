c026478b5c8f6a29521937b018e1eb84246e9c28
Max <61512470+Mevial@users.noreply.github.com>
2022-06-23 20:22:05 +0300

    fix import

--- src/app/pages/main-dashboard/categories/categories.module.ts
+++ src/app/pages/main-dashboard/categories/categories.module.ts
@@ -3,9 +3,9 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatListModule } from '@angular/material/list';
 import { NgModule } from '@angular/core';
 import { NgxSpinnerModule } from 'ngx-spinner';
+
 import { AdvertisementsListButtonsModule } from '../../../shared/components/advertisements-list/advertisement/advertisements-list-buttons/advertisements-list-buttons.module';
 import { AdvertisementsListModule } from '../../../shared/components/advertisements-list/advertisements-list.module';
-
 import { CategoriesComponent } from './categories.component';
 import { CategoriesService } from './categories.service';
 import { CategoryModule } from './category/category.module';
