c9cdead30fbc42ac55b477a1a97172431f1b18e5
ShpilevskyAndrei <103843433+ShpilevskyAndrei@users.noreply.github.com>
2022-06-28 16:35:27 +0300

    eslint

--- src/app/shared/components/advertisements-list/advertisement/advertisement-price/advertisement-price.module.ts
+++ src/app/shared/components/advertisements-list/advertisement/advertisement-price/advertisement-price.module.ts
@@ -8,7 +8,6 @@ registerLocaleData(localeFr, 'fr');
 
 import { AdvertisementPriceComponent } from './advertisement-price.component';
 import { AvailableDateFormatPipe } from '../../../../pipes/available-date-format.pipe';
-import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [AdvertisementPriceComponent, AvailableDateFormatPipe],
