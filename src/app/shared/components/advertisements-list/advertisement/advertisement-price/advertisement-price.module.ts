e2f770a2a0a0abf9d6bbd87d04141d02875ad452
ShpilevskyAndrei <103843433+ShpilevskyAndrei@users.noreply.github.com>
2022-06-28 15:52:32 +0300

    styles

--- src/app/shared/components/advertisements-list/advertisement/advertisement-price/advertisement-price.module.ts
+++ src/app/shared/components/advertisements-list/advertisement/advertisement-price/advertisement-price.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { registerLocaleData } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 import localeFr from '@angular/common/locales/fr';
 
 registerLocaleData(localeFr, 'fr');
@@ -10,7 +11,7 @@ import { AvailableDateFormatPipe } from '../../../../pipes/available-date-format
 
 @NgModule({
   declarations: [AdvertisementPriceComponent, AvailableDateFormatPipe],
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   exports: [AdvertisementPriceComponent, AvailableDateFormatPipe],
 })
 export class AdvertisementPriceModule {}
