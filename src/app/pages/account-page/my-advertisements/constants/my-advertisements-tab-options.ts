0f26d6fc267e0cdd9fd204355e0c42f87fd66dee
Max <61512470+Mevial@users.noreply.github.com>
2022-06-29 17:01:11 +0300

    add constant

--- src/app/pages/account-page/my-advertisements/constants/my-advertisements-tab-options.ts
+++ src/app/pages/account-page/my-advertisements/constants/my-advertisements-tab-options.ts
@@ -1,4 +1,8 @@
 export const ACTIVE_TAB = 'Active';
 export const PENDING_TAB = 'Pending';
 export const INACTIVE_TAB = 'Inactive';
-export const MY_ADVERTISEMENTS_TABS = [ACTIVE_TAB, PENDING_TAB, INACTIVE_TAB];
+export const MY_ADVERTISEMENTS_TABS: string[] = [
+  ACTIVE_TAB,
+  PENDING_TAB,
+  INACTIVE_TAB,
+];
