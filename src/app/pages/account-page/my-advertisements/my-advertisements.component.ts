b3d3cbd25bce1e16553d1dd010d784bac63a8286
Max <61512470+Mevial@users.noreply.github.com>
2022-06-29 17:01:57 +0300

    add tabs to my adv and switch ngif

--- src/app/pages/account-page/my-advertisements/my-advertisements.component.ts
+++ src/app/pages/account-page/my-advertisements/my-advertisements.component.ts
@@ -6,6 +6,7 @@ import {
   OnInit,
   Output,
 } from '@angular/core';
+import { MatTabChangeEvent } from '@angular/material/tabs';
 
 import { IAdvertisementRequestInterface } from '../../../shared/components/advertisements-list/interfaces/advertisement-request.interface';
 import { LoadingStatus } from '../../../shared/interfaces/loading-status';
@@ -13,6 +14,7 @@ import { IUser } from '../../../shared/interfaces/user.interface';
 import { AccountPageActions } from '../../../state/account-page/account-page.actions';
 import { UserRole } from '../../../shared/components/header/enums/user-role';
 import { IAdvertisementInterface } from '../../../shared/components/advertisements-list/interfaces/advertisement.interface';
+import { MY_ADVERTISEMENTS_TABS } from './constants/my-advertisements-tab-options';
 
 @Component({
   selector: 'app-my-advertisements',
@@ -26,11 +28,15 @@ export class MyAdvertisementsComponent implements OnInit {
   @Input() public myAdvertisementsLoadingStatus: LoadingStatus | null;
   @Input() public user: IUser | null;
   @Input() public userRole: UserRole | null;
+  @Input() public myAdvertisementsTabs = MY_ADVERTISEMENTS_TABS;
+  @Input() public selectMyAdvertisementTabTitle: string | null;
 
   @Output() public dispatcher: EventEmitter<Function> =
     new EventEmitter<Function>();
   @Output() public confirmDeal: EventEmitter<IAdvertisementInterface> =
     new EventEmitter<IAdvertisementInterface>();
+  @Output() public selectMyAdvertisementsTab: EventEmitter<string> =
+    new EventEmitter<string>();
 
   public ngOnInit(): void {
     this.dispatcher.emit(AccountPageActions.getMyAdvertisementsRequest);
@@ -38,5 +44,21 @@ export class MyAdvertisementsComponent implements OnInit {
 
   public onConfirmDeal(advertisement: IAdvertisementInterface): void {
     this.confirmDeal.emit(advertisement);
+  }
+
+  public onSelectMyAdvertisementTab(tabChangeEvent: MatTabChangeEvent): void {
+    this.selectMyAdvertisementsTab.emit(
+      this.myAdvertisementsTabs?.[tabChangeEvent.index]
+    );
+  }
+
+  public findMyAdvertisementTabTitle(): number {
+    if (!this.myAdvertisementsTabs || !this.selectMyAdvertisementTabTitle) {
+      return 0;
+    }
+
+    return this.myAdvertisementsTabs.findIndex((tab: string) => {
+      return tab === this.selectMyAdvertisementTabTitle;
+    });
   }
 }
