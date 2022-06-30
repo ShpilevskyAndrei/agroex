3cb36abfd9678fea6f7874926540285d71013f79
Max <61512470+Mevial@users.noreply.github.com>
2022-06-30 11:05:30 +0300

    add spinner to my orders and fix position

--- src/app/pages/account-page/my-orders/my-orders.module.ts
+++ src/app/pages/account-page/my-orders/my-orders.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { MyOrdersComponent } from './my-orders.component';
 
 @NgModule({
   declarations: [MyOrdersComponent],
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, NgxSpinnerModule],
   exports: [MyOrdersComponent],
 })
 export class MyOrdersModule {}
